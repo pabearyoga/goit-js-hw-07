c9b41b457bd3eb5a4cf6d649acae3fb489065482
Pabearyoga <pabearyoga@gmail.com>
2022-07-05 14:12:46 +0300

    fix 02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+const markup = galleryItems
+  .map(
+    (image) =>
+      `<a class="gallery__item" href="${image.original}">
+            <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+        </a>`
+  )
+  .join("");
+gallery.insertAdjacentHTML("beforeend", markup);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
